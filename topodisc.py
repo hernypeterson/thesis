d919155818296a2445f06ae9a08c9e48072a5785
hernypeterson <hgpa2018@mymail.pomona.edu>
2023-04-14 19:07:20 -0700

    nice figures

--- topodisc.py
+++ topodisc.py
@@ -57,14 +57,14 @@ def mogi_tilt(dist, epv, d, length_scale_mult=LENGTH_SCALE_MULT):
     return np.degrees(np.arctan2(num, denom))
 
 
-def epv_numerical_model(depth, radius, aspect, pmult):
+def epv_numerical_model(depth, radius, aspect, pmult) -> float:
 
     volume = (4/3) * np.pi * radius**3 * aspect
     pressure = ROCK_DENSITY * MARS_GRAVITY * depth * pmult
     return volume * pressure
 
 
-def great_circle_distance(lat1_deg, lon1_deg, lat2_deg, lon2_deg, radius=MARS_EQ_RADIUS):
+def great_circle_distance(lat1_deg, lon1_deg, lat2_deg, lon2_deg, radius=MARS_EQ_RADIUS) -> float:
     '''distance between pts 1 and 2'''
     lat1 = rad(lat1_deg)
     lon1 = rad(lon1_deg)
@@ -80,7 +80,7 @@ def great_circle_distance(lat1_deg, lon1_deg, lat2_deg, lon2_deg, radius=MARS_EQ
     return angular_distance * radius
 
 
-def great_circle_bearing(lat1_deg, lon1_deg, lat2_deg, lon2_deg):
+def great_circle_bearing(lat1_deg, lon1_deg, lat2_deg, lon2_deg) -> float:
     '''azimuth from pt 1 AWAY from pt 2'''
 
     lat1 = rad(lat1_deg)
@@ -95,13 +95,13 @@ def great_circle_bearing(lat1_deg, lon1_deg, lat2_deg, lon2_deg):
     return (180 + deg(arctan2(y, x))) % 360
 
 
-def signed_angular_difference(ang2_deg, ang1_deg):
+def signed_angular_difference(ang2_deg, ang1_deg) -> float:
     '''angular difference expressed in range -180 to 180'''
 
     return ((ang2_deg - ang1_deg + 180) % 360) - 180
 
 
-def angular_difference(ang2_deg, ang1_deg):
+def angular_difference(ang2_deg, ang1_deg) -> float:
     return np.abs(signed_angular_difference(ang2_deg, ang1_deg))
 
 
@@ -125,6 +125,45 @@ def great_circle_projection(beta_deg, slope_deg):
     return deg(proj)
 
 
+def best_beta1(
+    beta1_deg: float,
+    beta2_deg: float,
+    slope_deg: float,
+    paleo_azimuth_uncertainty: float
+):
+    # already within uncertainty -> tilt = 0
+    if np.abs(signed_angular_difference(
+            beta1_deg, beta2_deg
+    )) < paleo_azimuth_uncertainty:
+        return 0
+
+    # beta1 uncertainty boundaries
+    beta1_possible = np.array([
+        beta1_deg + paleo_azimuth_uncertainty,
+        beta1_deg - paleo_azimuth_uncertainty
+    ])
+
+    try:
+        possible_paleo_slopes = np.array([
+            paleo_slope(
+                beta1_deg=beta1,
+                beta2_deg=beta2_deg,
+                sl2_deg=slope_deg
+            )
+            for beta1 in beta1_possible
+        ])
+        possible_tilts = np.array([great_circle_projection(beta2_deg, slope_deg) \
+        - great_circle_projection(*paleo_pair) for paleo_pair in zip(beta1_possible, possible_paleo_slopes)])
+
+        best_beta1 = beta1_possible[np.nanargmin(np.abs(possible_tilts))]
+    except RuntimeWarning:  # raised if all NaNs
+        return np.nan
+    except ValueError:
+        return np.nan
+    
+    return best_beta1
+
+
 def minimum_tilt(
     beta1_deg: float,
     beta2_deg: float,
@@ -144,26 +183,25 @@ def minimum_tilt(
         beta1_deg - paleo_azimuth_uncertainty
     ])
 
-    nearest_beta1 = min(
-        beta1_possible,
-        key=lambda beta1_deg: angular_difference(beta1_deg, beta2_deg)
-    )
+    try:
+        possible_paleo_slopes = np.array([
+            paleo_slope(
+                beta1_deg=beta1,
+                beta2_deg=beta2_deg,
+                sl2_deg=slope_deg
+            )
+            for beta1 in beta1_possible
+        ])
+        possible_tilts = np.array([great_circle_projection(beta2_deg, slope_deg) \
+        - great_circle_projection(*paleo_pair) for paleo_pair in zip(beta1_possible, possible_paleo_slopes)])
 
-    best_paleo_slope = paleo_slope(
-        beta1_deg=nearest_beta1,
-        beta2_deg=beta2_deg,
-        sl2_deg=slope_deg
-    )
-
-    # no paleo_slope -> no tilt
-    if np.isnan(best_paleo_slope):
+        best_tilt = possible_tilts[np.nanargmin(np.abs(possible_tilts))]
+    except RuntimeWarning:  # raised if all NaNs
+        return np.nan
+    except ValueError:
         return np.nan
 
-    # evaluate tilt
-    tilt = great_circle_projection(beta2_deg, slope_deg) \
-        - great_circle_projection(nearest_beta1, best_paleo_slope)
-
-    return tilt
+    return best_tilt
 
 
 def is_tiltable(tilt, dist_m):
@@ -182,6 +220,7 @@ def is_tiltable(tilt, dist_m):
 
     return True
 
+
 def nan_if_untiltable(row):
 
     if is_tiltable(row['tilt'], row['dist']):
@@ -193,24 +232,28 @@ def nan_if_untiltable(row):
 
 
 def tilt_check(
-        test_az1: float,
-        test_az2: float,
-        test_sl2: float,
-        paleo_azimuth_uncertainty: float = 7,
-        max_abs_tilt: float = 5,
-        plot_resolution: float = 3,
-        plot_size: float = 5,
-        plot_radius: float = 0.1,
-        legend_position: tuple = (0.5, 0.5)
-    ):  # type: ignore
+    test_az1: float,
+    test_az2: float,
+    test_sl2: float,
+    paleo_azimuth_uncertainty: float = 7,
+    max_abs_tilt: float = 5,
+    plot_resolution: int = 3,
+    plot_size: float = 5,
+    plot_radius: float = 0.1,
+    legend_position: tuple = (0.5, 0.5),
+    full_tilt: bool = True,
+    example_bearing=None,
+):  # type: ignore
+    
+    is_discordant = angular_difference(test_az1, test_az2) > paleo_azimuth_uncertainty
 
-    test_bearing = np.arange(360 * plot_resolution) / plot_resolution
+    bearing_range = np.arange(360 * plot_resolution) / plot_resolution
 
-    test_rim = 0.9 * plot_radius * np.ones(len(test_bearing))
+    test_rim = 0.9 * plot_radius * np.ones(len(bearing_range))
 
     test_tilt = []
 
-    for bearing in test_bearing:
+    for bearing in bearing_range:
         this_beta1 = signed_angular_difference(
             ang2_deg=test_az1, ang1_deg=bearing)
         this_beta2 = signed_angular_difference(
@@ -253,46 +296,143 @@ def tilt_check(
               colors=['black'], label=f"$\\theta_1=$" + f"{test_az1}")
 
     # plot modern topographic attitude
-    sns.scatterplot(x=[np.radians(test_az2)], y=[np.sin(np.radians(test_sl2))],
-                    label='$\\theta_2=$' + f'{test_az2}, \n' + '$\\varphi_2=$' + f'{test_sl2}', color='black', s=100)
+    sns.scatterplot(
+        x=[np.radians(test_az2)],
+        y=[np.sin(np.radians(test_sl2))],
+        label='$\\theta_2=$'
+        + f'{test_az2}, \n'
+        + '$\\varphi_2=$'
+        + f'{test_sl2}',
+        color='black',
+        s=100,
+        zorder=4)
 
     # nice radial ticks
     ax.set_yticks(np.linspace(0, plot_radius, 3)[1:])
 
-    # plot calculated tilt by color around the rim. bearing + 180 to show direction TOWARD the center
-    for i in np.linspace(0.9, 1.1, 20):
-        if i == 0.9:
-            sns.scatterplot(
-                x=np.radians(test_bearing + 180),
-                y=test_rim,
-                hue=nptest_tilt,
-                linewidth=0,
-                palette='RdYlGn',
-                size=1,
-                # legend=False
-            )
-        else:
-            sns.scatterplot(
-                x=np.radians(test_bearing + 180),
-                y=i * test_rim,
-                hue=nptest_tilt,
-                linewidth=0,
-                palette='RdYlGn',
-                size=1,
-                legend=False  # type: ignore
-            )
+    if is_discordant:
+        palette = 'RdYlGn'
+    else:
+        palette = 'Blues'
 
-    # plt.title("Tilt required to explain a discordant sample \n by direction toward the center. All angles in degrees.")
-    # plt.legend(, loc='upper left')
+    # plot calculated tilt by color around the rim. bearing + 180 to show direction TOWARD the center
+    if full_tilt:
+        for i, r in enumerate(np.linspace(0.9, 1.2, 20 * plot_resolution)):
+            if i == 0:
+                sns.scatterplot(
+                    x=np.radians(bearing_range + 180),
+                    y=test_rim,
+                    hue=nptest_tilt,
+                    linewidth=0,
+                    palette=palette,
+                    size=1,
+                    # legend=False
+                )
+            else:
+                sns.scatterplot(
+                    x=np.radians(bearing_range + 180),
+                    y=r * test_rim,
+                    hue=nptest_tilt,
+                    linewidth=0,
+                    palette=palette,
+                    size=1,
+                    legend=False  # type: ignore
+                )
+
+    if example_bearing is not None:
+        plt.vlines(
+            x=np.radians(example_bearing),
+            ymin=0,
+            ymax=1,
+            label='To Center',
+            colors=['grey']
+        )
+        plt.vlines(
+            x=np.radians((
+                example_bearing + 90,
+                example_bearing - 90,
+            )),
+            ymin=0,
+            ymax=1,
+            linestyles='dashed',
+            colors=['grey', 'grey'],
+            label='Tilt Axis'
+        )
+
+        example_beta2 = signed_angular_difference(
+            ang2_deg=test_az2, ang1_deg=example_bearing + 180
+        )
+        example_beta1 = signed_angular_difference(
+            ang2_deg=test_az1, ang1_deg=example_bearing + 180
+        )
+
+        example_beta1_boundary = best_beta1(
+            beta1_deg=example_beta1,
+            beta2_deg=example_beta2,
+            slope_deg=test_sl2,
+            paleo_azimuth_uncertainty=paleo_azimuth_uncertainty
+        )
+
+        example_tilt = minimum_tilt(
+            beta1_deg=example_beta1,
+            beta2_deg=example_beta2,
+            slope_deg=test_sl2,
+            paleo_azimuth_uncertainty=paleo_azimuth_uncertainty
+        )
+
+        example_paleo_slope = paleo_slope(
+            beta1_deg=example_beta1_boundary,
+            beta2_deg=example_beta2,
+            sl2_deg=test_sl2
+        )
+
+        example_az1_boundary = example_bearing + 180 + example_beta1_boundary
+
+        tilt_path_az = np.radians([
+            example_az1_boundary, test_az2
+        ])
+
+        tilt_path_sl = np.sin(np.radians([
+            example_paleo_slope, test_sl2
+        ]))
+
+        sns.lineplot(
+            x=tilt_path_az,
+            y=tilt_path_sl,
+            label=f'Tilt: {round(example_tilt, 1)}'
+        )
+
+        sns.scatterplot(
+            x=[np.radians(example_az1_boundary)],
+            y=[np.sin(np.radians(example_paleo_slope))],
+            label='$\\theta_1=$'
+            + f'{test_az1}, \n'
+            + '$\\varphi_1=$'
+            + f'{round(example_paleo_slope, 1)}',
+            color='black',
+            s=100,
+            marker='X',
+            zorder=5)
+
     handles, labels = ax.get_legend_handles_labels()
 
+    # label az1 center before uncertainty
+    handles[0], handles[1] = handles[1], handles[0]
+    labels[0], labels[1] = labels[1], labels[0]
+
     # this is a hack to remove the "1" that appears randomly in the legend in the for loop implementation of thick rim
-    handles = handles[:-1]
-    labels = labels[:-1]
+
+    if full_tilt:
+        if is_discordant:
+            del(handles[8])
+            del(labels[8])
+        else:
+            del(handles[-1])
+            del(labels[-1])
 
     ax.legend(
-        handles[::-1],
-        labels[::-1],
+        handles,
+        labels,
         loc='center',
         framealpha=1,
         bbox_to_anchor=legend_position
@@ -310,12 +450,15 @@ def plot_envelope(max_dist_km: float = 100_000, has_label: bool = True, color: s
     dist_km = dist_m / 1000
 
     if has_label:
-        label = 'log$|E_{PV}\ /\ J|: $' + f'{np.round(np.log10(MAX_EPV), 1)}, ' + '$d: $' + f'{TEST_D/1000} km'
+        label = 'log$|E_{PV}\ /\ J|: $' + \
+            f'{np.round(np.log10(MAX_EPV), 1)}, ' + \
+            '$d: $' + f'{TEST_D/1000} km'
     else:
         label = None
 
-    sns.lineplot(x=dist_km,y=max_tilt, c=color, label=label)
-    sns.lineplot(x=dist_km,y=min_tilt, c=color)
+    sns.lineplot(x=dist_km, y=max_tilt, c=color, label=label)
+    sns.lineplot(x=dist_km, y=min_tilt, c=color)
+
 
 def plot_tilt_distance_dataset(df: pd.DataFrame, color=None, name: str = ''):
 
@@ -427,7 +570,8 @@ class Center:
 
             spot_check = self.get_data_subset(pop.sIDs)
             if exclude_untiltable:
-                spot_check['tilt'] = spot_check.apply(nan_if_untiltable, axis=1)
+                spot_check['tilt'] = spot_check.apply(
+                    nan_if_untiltable, axis=1)
 
             plot_tilt_distance_dataset(
                 spot_check,
@@ -473,6 +617,7 @@ def fit_mogi_function(df: pd.DataFrame, full_output: bool = False):
 
     return fit
 
+
 def frac_tiltable(df: pd.DataFrame) -> dict:
     score = {'frac_tiltable': np.nan}
     try:
@@ -483,10 +628,11 @@ def frac_tiltable(df: pd.DataFrame) -> dict:
         pass
     return score
 
+
 def inflation_score(df: pd.DataFrame) -> dict:
 
     # df_subset = df.loc[df['is_tiltable']]
-    df_subset = df.loc[df['tilt'].notnull()] # no envelope version
+    df_subset = df.loc[df['tilt'].notnull()]  # no envelope version
 
     # initialize output
     scores = {
@@ -631,7 +777,7 @@ def read_model_data(params: dict):
 class NumericalModel:
     params: dict
     pos1: np.array = field(repr=False)  # type: ignore
-    disp: np.array = field(repr=False) # type: ignore
+    disp: np.array = field(repr=False)  # type: ignore
     rmse: float = np.nan
 
     def __post_init__(self):
@@ -695,9 +841,9 @@ def make_numerical_model(params: dict):
 
 
 def numerical_model_rmse(
-        model: NumericalModel,
-        map_tilt_df: pd.DataFrame
-    ) -> float:
+    model: NumericalModel,
+    map_tilt_df: pd.DataFrame
+) -> float:
 
     possible_subset = map_tilt_df[map_tilt_df['tilt'].notnull()]
 
@@ -708,7 +854,7 @@ def numerical_model_rmse(
     dist_km_numerical_model = model.data['dist_km'].tolist()
 
     tilt_predicted = np.interp(
-        x=dist_km_map, # type: ignore
+        x=dist_km_map,  # type: ignore
         xp=dist_km_numerical_model,
         fp=tilt_numerical_model
     )
@@ -718,7 +864,7 @@ def numerical_model_rmse(
         y_pred=tilt_predicted
     ))
 
-    return rmse # type: ignore
+    return rmse  # type: ignore
 
 
 @dataclass
@@ -752,7 +898,8 @@ class ParamSweep:
 
         for model in self.models:
             try:
-                model.rmse = numerical_model_rmse(model, data_no_nulls) # type: ignore
+                model.rmse = numerical_model_rmse(
+                    model, data_no_nulls)  # type: ignore
             except:
                 pass
 
